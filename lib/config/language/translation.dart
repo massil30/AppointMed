f5e60832ccc9615eff0fc40c3183b495d84aae72
massil30 <massilattaf04@gmail.com>
2025-10-07 12:58:12 +0100

    change

---
+++ lib/config/language/translation.dart
@@ -0,0 +1,16 @@
+final Map<String, Map<String, String>> translations = {
+  'en': {
+    'welcome': 'Hi, WelcomeBack',
+    'hello': 'Hello!',
+    'choose_language': 'Choose a language:',
+    'english': 'English',
+    'french': 'French',
+  },
+  'fr': {
+    'welcome': 'Bienvenue',
+    'hello': 'Bonjour !',
+    'choose_language': 'Choisissez une langue :',
+    'english': 'Anglais',
+    'french': 'Français',
+  },
+};
